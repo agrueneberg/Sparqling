e019e40718296e7d164e0a23888bf1d1bc49d30b
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-08-17 09:36:30 -0500

    Display query result.

--- scripts/app.js
+++ scripts/app.js
@@ -28,13 +28,30 @@
     sparql.controller("query", function ($scope, rdfstore) {
         $scope.queryString = "SELECT * WHERE {?s ?p ?o .}";
         $scope.sparqlResult = [];
+        $scope.sparqlResultVariables = [];
         $scope.submitQuery = function () {
             try {
                 rdfstore.getStore(function (store) {
                     store.execute($scope.queryString, function (success, result) {
+                        var variables, normalizedResult;
                         if (success === true) {
                             if (Array.isArray(result) === true) {
-                                $scope.sparqlResult = result;
+                                if (result.length > 0) {
+                                 // Extract variable names.
+                                    variables = Object.keys(result[0]);
+                                 // Normalize result.
+                                    normalizedResult = [];
+                                    result.forEach(function (binding) {
+                                        var normalizedBinding;
+                                        normalizedBinding = [];
+                                        variables.forEach(function (variable) {
+                                            normalizedBinding.push(binding[variable]);
+                                        });
+                                        normalizedResult.push(normalizedBinding);
+                                    });
+                                    $scope.sparqlResult = normalizedResult;
+                                    $scope.sparqlResultVariables = variables;
+                                }
                             }
                         } else {
                             console.error(result);
